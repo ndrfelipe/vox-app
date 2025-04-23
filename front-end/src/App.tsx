918f7299c1149079b4008c0e323e31de4ae650be
victor <vfm@poli.br>
2025-04-23 16:04:51 +0000

    components file'

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,11 +1,5 @@
 function App() {
-  return (
-    <>
-      <div className="bg-black w-full h-screen flex justify-center items-center text-white">
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      </div>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
