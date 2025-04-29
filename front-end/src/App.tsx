b52e82bd8c7b2fd20d44cff3f65477a97e2ef627
Alan <164885084+alancodex@users.noreply.github.com>
2025-04-29 19:56:16 +0000

    update fontsize

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,19 +1,30 @@
-import Chatbox from "./components/ChatBox";
+import Chatbox from "./components/Chatbox";
 import Sidebar, { SidebarItem } from "./components/Sidebar";
+import FontSizeControls from "./components/FontSize";
+import { useState } from 'react';
 
-export default function App() {
-  return (
-    <div className="flex ">
-      <Sidebar>
-        <SidebarItem
-          icon={<i className="fas fa-home"></i>}
-          text="Inicio"
-          active
-        />
-        <SidebarItem icon={<i className="fas fa-user"></i>} text="Histórico" />
-      </Sidebar>
-      <Chatbox />
-    </div>
-  );
-}
+const App: React.FC = () => {
+    const [isLarge, setIsLarge] = useState(false);
 
+    return (
+        <div className={`flex ${isLarge ? 'text-xl [&_button]:text-xl [&_input]:text-xl [&_textarea]:text-xl' : 'text-base'}`}>
+            <Sidebar>
+                <SidebarItem
+                    icon={<i className="fas fa-home"></i>}
+                    text="Inicio"
+                    active
+                />
+                <SidebarItem 
+                    icon={<i className="fas fa-user"></i>} 
+                    text="Histórico" 
+                />
+            </Sidebar>
+            <Chatbox />
+            <FontSizeControls 
+                onSizeChange={setIsLarge} 
+            />
+        </div>
+    );
+};
+
+export default App;
