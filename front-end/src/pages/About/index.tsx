a46ac00ccfdca245dfe0291fb07918424f906192
victor <vfm@poli.br>
2025-05-15 23:29:18 -0300

    delete old front-end directory

--- front-end/src/pages/About/index.tsx
+++
@@ -1,8 +0,0 @@
-export default function About(){
-    return(
-        <main>
-            <h1>Olá sobre</h1>
-        </main>
-        
-    )
-}
--- front-end/src/pages/Contact/index.tsx
+++
@@ -1,5 +0,0 @@
-export default function Contact(){
-    return(
-        <h1>Olá contato</h1>
-    )
-}
